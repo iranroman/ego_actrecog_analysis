7b8f378aedcf1e93d5e0a8ec032a8956e751eae3
iranroman <iran@ccrma.stanford.edu>
2023-01-24 15:43:43 -0500

    remove overwritten function

--- slowfast/datasets/utils.py
+++ slowfast/datasets/utils.py
@@ -63,39 +63,6 @@ def pack_pathway_output(cfg, frames):
     """
     Prepare output as a list of tensors. Each tensor corresponding to a
     unique pathway.
-    Args:
-        frames (tensor): frames of images sampled from the video. The
-            dimension is `channel` x `num frames` x `height` x `width`.
-    Returns:
-        frame_list (list): list of tensors with the dimension of
-            `channel` x `num frames` x `height` x `width`.
-    """
-    #if cfg.MODEL.ARCH in cfg.MODEL.SINGLE_PATHWAY_ARCH:
-    #    frame_list = [frames]
-    #elif cfg.MODEL.ARCH in cfg.MODEL.MULTI_PATHWAY_ARCH:
-    fast_pathway = frames
-    # Perform temporal sampling from the fast pathway.
-    slow_pathway = torch.index_select(
-        frames,
-        1,
-        torch.linspace(
-            0, frames.shape[1] - 1, frames.shape[1] // cfg.SLOWFAST.ALPHA
-        ).long(),
-    )
-    frame_list = [slow_pathway, fast_pathway]
-    #else:
-    #    raise NotImplementedError(
-    #        "Model arch {} is not in {}".format(
-    #            cfg.MODEL.ARCH,
-    #            cfg.MODEL.SINGLE_PATHWAY_ARCH + cfg.MODEL.MULTI_PATHWAY_ARCH,
-    #        )
-    #    )
-    return frame_list
-
-def pack_pathway_output(cfg, frames):
-    """
-    Prepare output as a list of tensors. Each tensor corresponding to a
-    unique pathway.
 
     Input/output frame dimensions are:
         for video: `channel` x `num frames` x `height` x `width`
